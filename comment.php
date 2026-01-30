ef02e9f286f8a14cba871a6725aa8dbca21f6437
saramussa2727-max <saramussa2727@gmail.com>
2026-01-30 17:38:42 +0300

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,6 +1,6 @@
 <?php
-require_once "../../middleware/auth.php";
-require_once "../../config/database.php";
+require_once "../middleware/auth.php";
+require_once "../config/database.php";
 
 $user = authRequired();
 $data = json_decode(file_get_contents("php://input"), true);
@@ -11,3 +11,4 @@ $stmt = $pdo->prepare(
 $stmt->execute([$user['id'],$data['media_id'],$data['comment']]);
 
 jsonResponse(["message"=>"Comment added"]);
+
